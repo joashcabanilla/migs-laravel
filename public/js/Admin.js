d4588aaf3140a4418de6ce590cc8014a85913e8e
joashcabanilla <joash0199@gmail.com>
2024-02-29 11:01:47 +0800

    push

--- public/js/Admin.js
+++ public/js/Admin.js
@@ -300,14 +300,14 @@ const MemberInfoTab = () => {
         serverSide: true,
         dom: 'rtip',
         columnDefs: [
-            { targets: 0, width: '1%', className: "text-center font-weight-bold p-2" },
+            { targets: 0, width: '5%', className: "text-center font-weight-bold p-2" },
             { targets: 1, width: '10%', className: "text-center font-weight-bold p-2" },
             { targets: 2, width: '10%', className: "text-center font-weight-bold p-2" },
             { targets: 3, width: '30%', className: "text-left font-weight-bold p-2" },
             { targets: 4, width: '15%', className: "text-center font-weight-bold p-2" },
             { targets: 5, width: '15%', className: "text-center font-weight-bold p-2" },
             { targets: 6, width: '10%', className: "text-center font-weight-bold p-2" },
-            { targets: 7, width: '9%', className: "text-center font-weight-bold p-2" },
+            { targets: 7, width: '5%', className: "text-center font-weight-bold p-2" },
         ],
         ajax: {
             url: 'admin/MemberDataTable',
