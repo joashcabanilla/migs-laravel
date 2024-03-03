316ff7cfa9393cd2644d373f9f411760594c3451
joashcabanilla <joash0199@gmail.com>
2024-03-03 18:11:44 +0800

    push

--- resources/views/Layouts/Admin.blade.php
+++ resources/views/Layouts/Admin.blade.php
@@ -223,6 +223,6 @@
     </script>
     
     {{--script for page --}}
-    <script src="{{asset('js/Admin.js')}}"></script>
-    <script src="{{asset('js/Sidebar.js')}}"></script>
+    <script src="{{URL::asset('js/Admin.js')}}"></script>
+    <script src="{{URL::asset('js/Sidebar.js')}}"></script>
 </html>
