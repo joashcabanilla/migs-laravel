3348c35b5513965d6083e902f24b1aa311d688da
joashcabanilla <joash0199@gmail.com>
2024-03-04 16:28:10 +0800

    update

--- resources/views/Report/ElectionSummary.php
+++ resources/views/Report/ElectionSummary.php
@@ -4,7 +4,7 @@
     $defaultFont = base_path("app/Includes/fonts/Calibri.afm"); 
     $h = $pdf->ez['pageHeight'];
     $w = $pdf->ez['pageWidth'];
-    $fontSize = 15;
+    $fontSize = 13;
     $startW = $pdf->C2P(0.5);
     $rowH = $pdf->C2P(0.5);
     $startH = $h-$startW;
@@ -15,7 +15,7 @@
 
     $picture = base_path('public/image/letterhead.jpg');
     $pdf->addJpegFromFile($picture,$startW,$startH-$pdf->C2P(3.3),$endW,$pdf->C2P(3.5));
-    $startH-=$rowH+$pdf->C2P(3.7);
+    $startH-=$rowH+$pdf->C2P(3.5);
     $pdf->addTextWrap($startW, $startH, $endW, $fontSize, "<b>NOVADECI ELECTION 2024</b>",'center');
     $startH-=$rowH+$pdf->C2P(0.2);
     $pdf->addTextWrap($startW, $startH, $endW, $fontSize, "<b>VOTES TALLY AS OF ".strtoupper($DateTime)."</b>",'center');
@@ -24,7 +24,7 @@
     $extend = $pdf->C2P(0.015);
     $pdf->setLineStyle(1);
 
-    $rowH = $pdf->C2P(1);
+    $rowH = $pdf->C2P(0.8);
     $startW += $rowH;
     $endW -= $rowH;
     foreach($votesTally as $position => $candidates){
@@ -47,12 +47,12 @@
         //bottom
         $pdf->line($startW-$extend,$startH-$rowH,$endW+$extend,$startH-$rowH);
         $fontSize = 15;
-        $pdf->addTextWrap($startW, $startH+$pdf->C2P(0.3), $endW, $fontSize, "<b>".$position."</b>",'center');
+        $pdf->addTextWrap($startW, $startH+$pdf->C2P(0.25), $endW, $fontSize, "<b>".$position."</b>",'center');
         $startH-=$rowH;
 
         $fontSize = 13;
-        $pdf->addTextWrap($startW+$pdf->C2P(0.3), $startH+$pdf->C2P(0.3), $pdf->C2P(13.8), $fontSize, "<b>CANDIDATES</b>",'left');
-        $pdf->addTextWrap($startW+$pdf->C2P(14), $startH+$pdf->C2P(0.3),$pdf->C2P(4.1), $fontSize, "<b>VOTES</b>",'center');
+        $pdf->addTextWrap($startW+$pdf->C2P(0.3), $startH+$pdf->C2P(0.25), $pdf->C2P(13.8), $fontSize, "<b>CANDIDATES</b>",'left');
+        $pdf->addTextWrap($startW+$pdf->C2P(14), $startH+$pdf->C2P(0.25),$pdf->C2P(4.1), $fontSize, "<b>VOTES</b>",'center');
 
         foreach($candidates as $name => $candidate){
             //left corner
@@ -66,11 +66,27 @@
             $startH-=$rowH;
 
             $fontSize = 13;
-            $pdf->addTextWrap($startW+$pdf->C2P(0.3), $startH+$pdf->C2P(0.3), $pdf->C2P(13.8), $fontSize, strtoupper($name),'left');
+            $pdf->addTextWrap($startW+$pdf->C2P(0.3), $startH+$pdf->C2P(0.25), $pdf->C2P(13.8), $fontSize, strtoupper($name),'left');
             $votes = gettype($candidate) == "array" ? number_format(count($candidate)) : number_format($candidate);
-            $pdf->addTextWrap($startW+$pdf->C2P(14), $startH+$pdf->C2P(0.3),$pdf->C2P(4.1), $fontSize, $votes,'center');
+            $pdf->addTextWrap($startW+$pdf->C2P(14), $startH+$pdf->C2P(0.25),$pdf->C2P(4.1), $fontSize, $votes,'center');
         }
-        $startH-=$pdf->C2P(0.5);
+        $startH-=$pdf->C2P(0.25);
     }
+
+    $startH-=$pdf->C2P(0.25);
+    $startH-=$rowH;
+    $pdf->line($startW-$extend,$startH,$pdf->C2P(8),$startH);
+    $startH-=$rowH;
+    $pdf->addTextWrap($startW, $startH+$pdf->C2P(0.25), $pdf->C2P(4), $fontSize, "<b>Elecom Chairman</b>",'left');
+
+    $startH-=$rowH;
+    $pdf->line($startW-$extend,$startH,$pdf->C2P(8),$startH);
+    $startH-=$rowH;
+    $pdf->addTextWrap($startW, $startH+$pdf->C2P(0.25), $pdf->C2P(4), $fontSize, "<b>Audit Chairman</b>",'left');
+
+    $startH-=$rowH;
+    $pdf->line($startW+$pdf->C2P(12),$startH,$endW,$startH);
+    $startH-=$rowH;
+    $pdf->addTextWrap($startW+$pdf->C2P(12), $startH+$pdf->C2P(0.25), $pdf->C2P(4), $fontSize, "<b>MIS Manager</b>",'left');
     $pdf->ezStream();
 ?>
