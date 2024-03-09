823012d017461204c5752ac04b99a44df50ae4da
joashcabanilla <joash0199@gmail.com>
2024-03-09 12:43:12 +0800

    update

---
+++ resources/views/Report/Excel/ElectionSummaryPerMember.php
@@ -0,0 +1,110 @@
+<?php
+    error_reporting(0);
+    require_once(app_path('Includes/excel/spreadsheet/Writer.php'));
+    $xls = new Spreadsheet_Excel_Writer();
+    $header = $xls->addFormat(array('Size' => 11));
+    $header->setLocked();
+    $header->setBold();
+    $header->setFontFamily('Arial');
+    $header->setAlign("center");
+    $header->setAlign("vcenter");
+
+    $subheader = $xls->addFormat(array('Size' => 10));
+    $subheader->setLocked();
+    $subheader->setFontFamily('Arial');
+    $subheader->setAlign("center");
+    $subheader->setAlign("vcenter");
+ 
+    $subheaderB = $xls->addFormat(array('Size' => 10));
+    $subheaderB->setLocked();
+    $subheaderB->setFontFamily('Arial');
+    $subheaderB->setAlign("center");
+    $subheaderB->setAlign("vcenter");
+    $subheaderB->setBold();
+    $subheaderB->setBorder(1);
+
+    $subheaderName = $xls->addFormat(array('Size' => 10));
+    $subheaderName->setLocked();
+    $subheaderName->setFontFamily('Arial');
+    $subheaderName->setAlign("left");
+    $subheaderName->setAlign("vcenter");
+    $subheaderName->setBold();
+
+    $normal = $xls->addFormat(array('Size' => 10));
+    $normal->setFontFamily('Arial');
+    $normal->setAlign("left");
+    $normal->setAlign("vcenter");
+    $normal->setTextWrap();
+    $normal->setLocked();
+    $normal->setBorder(1);
+    
+    $normalC = $xls->addFormat(array('Size' => 10));
+    $normalC->setFontFamily('Arial');
+    $normalC->setAlign("center");
+    $normalC->setAlign("vcenter");
+    $normalC->setLocked();
+    $normalC->setBorder(1);
+    
+    $normalR = $xls->addFormat(array('Size' => 10));
+    $normalR->setFontFamily('Arial');
+    $normalR->setAlign("right");
+    $normalR->setAlign("vcenter");
+    $normalR->setBold();
+    $normalR->setLocked();
+    $normalR->setBorder(1);
+
+    $title = "Election Summary";
+    $sheet = $xls->addWorksheet($title);
+    $xls->send($title.".xls");
+
+    $fields = array(
+        array('Pb No',20),
+        array('Member ID',20),
+        array('NAME',40),
+        array('BRANCH',20),
+        array('VOTE METHOD',20),
+        array('DATE',20)
+    );
+
+    $c = $r = 0;
+    $fieldCount =  count($fields)-1;
+    $rowHeaderH = 20;
+    $sheet->setRow($r,$rowHeaderH);
+    $sheet->write($r,$c,"ELECTION SUMMARY",$header);    
+    $sheet->mergeCells($r,$c,$r,$fieldCount);
+    $r++;
+
+    $sheet->setRow($r,$rowHeaderH);
+    $sheet->write($r,$c,"TOTAL VOTED: ".number_format(count($electionSummary)),$header);    
+    $sheet->mergeCells($r,$c,$r,$fieldCount);
+    $r++;
+
+    $sheet->setRow($r,$rowHeaderH);
+    $sheet->write($r,$c,"AS OF ".$DateTime,$header);    
+    $sheet->mergeCells($r,$c,$r,$fieldCount);
+    $r+=2;
+
+    foreach($fields as $fieldinfo):
+        list($caption,$colwidth) = $fieldinfo;
+        $sheet->setRow($r,$rowHeaderH);
+        $sheet->setColumn($c,$c,$colwidth);
+        $sheet->write($r,$c,$caption,$subheaderB);$c++;
+    endforeach;
+    $r++;
+
+    foreach($electionSummary as $data){
+        $c = 0;
+        $sheet->setRow($r,$rowHeaderH);
+        $sheet->write($r,$c,$data["pbno"],$normalC);$c++;
+        $sheet->write($r,$c,$data["memberId"],$normalC);$c++;
+        $sheet->write($r,$c,$data["name"],$normal);$c++;
+        $sheet->write($r,$c,$data["branch"],$normalC);$c++;
+        $sheet->write($r,$c,$data["voteMethod"],$normalC);$c++;
+        $sheet->write($r,$c,$data["dateTime"],$normalC);$c++;
+        $r++;
+    }
+    
+    $xls->close();
+    die;
+?>
+    
--- resources/views/Report/Excel/GaItemsSummary.php
+++ resources/views/Report/Excel/GaItemsSummary.php
@@ -2,7 +2,7 @@
     error_reporting(0);
     require_once(app_path('Includes/excel/spreadsheet/Writer.php'));
     $xls = new Spreadsheet_Excel_Writer();
-    $header = $xls->addFormat(array('Size' => 12));
+    $header = $xls->addFormat(array('Size' => 11));
     $header->setLocked();
     $header->setBold();
     $header->setFontFamily('Arial');
@@ -21,6 +21,7 @@
     $subheaderB->setAlign("center");
     $subheaderB->setAlign("vcenter");
     $subheaderB->setBold();
+    $subheaderB->setBorder(1);
 
     $subheaderName = $xls->addFormat(array('Size' => 10));
     $subheaderName->setLocked();
@@ -35,12 +36,14 @@
     $normal->setAlign("vcenter");
     $normal->setTextWrap();
     $normal->setLocked();
+    $normal->setBorder(1);
     
     $normalC = $xls->addFormat(array('Size' => 10));
     $normalC->setFontFamily('Arial');
     $normalC->setAlign("center");
     $normalC->setAlign("vcenter");
     $normalC->setLocked();
+    $normalC->setBorder(1);
     
     $normalR = $xls->addFormat(array('Size' => 10));
     $normalR->setFontFamily('Arial');
@@ -48,90 +51,60 @@
     $normalR->setAlign("vcenter");
     $normalR->setBold();
     $normalR->setLocked();
+    $normalR->setBorder(1);
 
     $title = "GA Items Summary";
     $sheet = $xls->addWorksheet($title);
     $xls->send($title.".xls");
 
     $fields = array(
-        array('#',10),
-        array('Subject Code',20),
-        array('Subject Description',40),
-        array('Section Code',20),
-        array('Units',10),
-        array('Hours',10),
-        array('Schedule',40),
-        array("Student Count",15),
-        array("Rooms",15)
+        array('USERNAME',40),
+        array('BRANCH',40),
+        array('VOTE METHOD',20),
+        array('TOTAL REGISTERED',20),
+        array('DATE',20)
     );
 
     $c = $r = 0;
     $fieldCount =  count($fields)-1;
     $rowHeaderH = 20;
-    // $sheet->setRow($r,$rowHeaderH);
-    // $sheet->write($r,$c,$schoolName,$header);    
-    // $sheet->mergeCells($r,$c,$r,$fieldCount);
-    // $r++;
+    $sheet->setRow($r,$rowHeaderH);
+    $sheet->write($r,$c,"GA ITEMS SUMMARY REPORT",$header);    
+    $sheet->mergeCells($r,$c,$r,$fieldCount);
+    $r++;
 
-    // $sheet->setRow($r,$rowHeaderH);
-    // $sheet->write($r,$c,$schoolAddress,$subheader);    
-    // $sheet->mergeCells($r,$c,$r,$fieldCount);
-    // $r+=2;
+    $sheet->setRow($r,$rowHeaderH);
+    $sheet->write($r,$c,"TOTAL REGISTERED: " . number_format(count($memberList)),$header);    
+    $sheet->mergeCells($r,$c,$r,$fieldCount);
+    $r++;
 
-    // $sheet->setRow($r,$rowHeaderH);
-    // $sheet->write($r,$c,$title,$header);    
-    // $sheet->mergeCells($r,$c,$r,$fieldCount);
-    // $r++;
+    $sheet->setRow($r,$rowHeaderH);
+    $sheet->write($r,$c,"AS OF " . $DateTime,$header);    
+    $sheet->mergeCells($r,$c,$r,$fieldCount);
+    $r+=2;
 
-    // $sheet->setRow($r,$rowHeaderH);
-    // $sheet->write($r,$c,"Date Printed ".$datePrinted,$subheader);    
-    // $sheet->mergeCells($r,$c,$r,$fieldCount);
-    // $r+=2;
+    foreach($fields as $fieldinfo):
+        list($caption,$colwidth) = $fieldinfo;
+        $sheet->setRow($r,$rowHeaderH);
+        $sheet->setColumn($c,$c,$colwidth);
+        $sheet->write($r,$c,$caption,$subheaderB);$c++;
+    endforeach;
+    $r++;
 
-    // foreach($fields as $fieldinfo):
-    //     list($caption,$colwidth) = $fieldinfo;
-    //     $sheet->setRow($r,$rowHeaderH);
-    //     $sheet->setColumn($c,$c,$colwidth);
-    //     $sheet->write($r,$c,$caption,$subheaderB);$c++;
-    // endforeach;
-    // $r++;
-    // $counter = 1;
-    // foreach($reportData as $facultyName => $facultyLoad){
-    //     $c = 0;
-    //     list($fName,$fCode) = explode("|",$facultyName);
-    //     $sheet->setRow($r,$rowHeaderH);
-    //     $sheet->write($r,$c,$counter,$subheaderB);$c++;
-    //     $sheet->write($r,$c," ".$fCode . "    " .$fName,$subheaderName);$c++;
-    //     $counter++;
-    //     $r++;
-
-    //     $totalUnits = 0;
-    //     $totalHours = 0;
-
-    //     foreach($facultyLoad as $flData){
-    //         $c = 1;
-    //         if(strlen($flData['SubjDesc']) <= 50){
-    //             $sheet->setRow($r,$rowHeaderH);
-    //         }
-    //         $sheet->write($r,$c,$flData['SubjCode'],$normalC);$c++;
-    //         $sheet->write($r,$c,$flData['SubjDesc'],$normal);$c++;
-    //         $sheet->write($r,$c,$flData['SectCode'],$normal);$c++;
-    //         $sheet->write($r,$c,$flData['Units'],$normalC);$c++;
-    //         $sheet->write($r,$c,$flData['Hours'],$normalC);$c++;
-    //         $sheet->write($r,$c,$flData['Schedule'],$normal);$c++;
-    //         $sheet->write($r,$c,$flData['StudentCount'],$normalC);$c++;
-    //         $sheet->write($r,$c,$flData['Rooms'],$normalC);$c++;
-    //         $r++;
-    //         $totalUnits += $flData['Units'];
-    //         $totalHours += $flData['Hours'];
-    //     }
-    //     $c = 3;
-    //     $sheet->setRow($r,$rowHeaderH);
-    //     $sheet->write($r,$c,"Total:",$normalR);$c++;
-    //     $sheet->write($r,$c,$totalUnits,$subheaderB);$c++;
-    //     $sheet->write($r,$c,$totalHours,$subheaderB);$c++;
-    //     $r++;
-    // }
+    foreach($SummaryReport as $username => $voteMethod){
+        foreach($voteMethod as $method => $dates){
+            foreach($dates as $date => $count){
+                $c = 0;
+                $sheet->setRow($r,$rowHeaderH);
+                $sheet->write($r,$c,$username,$normal);$c++;
+                $sheet->write($r,$c,$branchUserList[$username],$normalC);$c++;
+                $sheet->write($r,$c,$method,$normalC);$c++;
+                $sheet->write($r,$c,number_format(count($count)),$normalC);$c++;
+                $sheet->write($r,$c,$date,$normalC);$c++;
+                $r++;
+            }
+        }
+    }
     $xls->close();
     die;
 ?>
