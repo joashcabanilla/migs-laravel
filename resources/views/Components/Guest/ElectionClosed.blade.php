4504be30fd2bf2b3d3cc63009b3fade7fbe4b773
joashcabanilla <joash0199@gmail.com>
2025-01-15 09:30:29 +0800

    maintenance

--- resources/views/Components/Guest/ElectionClosed.blade.php
+++ resources/views/Components/Guest/ElectionClosed.blade.php
@@ -10,12 +10,14 @@
                     <h1 class="font-weight-bold text-monospace text-center">The 48th General Assembly System is</h1>
                     {{-- <h1 class="font-weight-bold text-monospace text-center">The 48th PRE-GA Online Registration and Voting System is</h1> --}}
                     {{-- <h1 class="font-weight-bold text-monospace text-center" style="color:purple">accessible only from 7:00 AM to 10:00 PM</h1>  --}}
-                    <h1 class="font-weight-bold text-monospace text-center" style="color:purple">OFFICIALLY CLOSED.</h1> 
+                    {{-- <h1 class="font-weight-bold text-monospace text-center" style="color:purple">OFFICIALLY CLOSED.</h1>  --}}
+                    <h1 class="font-weight-bold text-monospace text-center" style="color:purple">UNDER MAINTENANCE.</h1>
                 </div>
             @else
                 <div class="card-body">
                     <h1 class="font-weight-bold text-monospace text-center">The 49th General Assembly System is</h1>
-                    <h1 class="font-weight-bold text-monospace text-center" style="color:purple">OFFICIALLY CLOSED.</h1> 
+                    {{-- <h1 class="font-weight-bold text-monospace text-center" style="color:purple">OFFICIALLY CLOSED.</h1>  --}}
+                    <h1 class="font-weight-bold text-monospace text-center" style="color:purple">UNDER MAINTENANCE.</h1>
                 </div>
             @endif
 
