1bda655bc35c7daef5e846dffa83e9f3587e019c
joashcabanilla <joash0199@gmail.com>
2025-03-12 12:37:56 +0800

    update

--- resources/views/Report/PDF/PrintTicket.php
+++ resources/views/Report/PDF/PrintTicket.php
@@ -12,6 +12,7 @@
     $pdf->selectFont($defaultFont);
 
     function generateLine($pdf, $top, $col, $w1, $w2, $h1, $h2){
+        $pdf->setLineStyle(3);
         $extend = $pdf->C2P(0.015);
 
         if($top == 1){
@@ -33,11 +34,12 @@
     }
 
     function generateData($pdf, $startW, $infoH, $ticketW, $ticket){
+        $pdf->setLineStyle(1);
         $fontSize = 65;
         $ticket = (object) $ticket;
         $label48 = base_path('public/image/49.jpg');
         $infoH-= $pdf->C2P(4.3);   
-        $pdf->addJpegFromFile($label48,$startW+$pdf->C2P(0.5),$infoH,$pdf->C2P(7.5),$pdf->C2P(3));
+        $pdf->addJpegFromFile($label48,$startW+$pdf->C2P(0.8),$infoH+$pdf->C2P(0.2),$pdf->C2P(7.5),$pdf->C2P(3));
         $infoH+=$pdf->C2P(4.3);
         
         $fontSize = 11;
@@ -45,21 +47,21 @@
         //ticket no
        $pdf->setColor(255,0.00,0.00);
         $infoH -= $pdf->C2P(0.5); 
-        $pdf->addTextWrap($startW, $infoH, $ticketW-$pdf->C2P(0.2), $fontSize, "<b>".$ticket->ticketNo."</b>",'right');
+        $pdf->addTextWrap($startW, $infoH-$pdf->C2P(0.4), $ticketW-$pdf->C2P(0.6), $fontSize+3, "<b>".$ticket->ticketNo."</b>",'right');
 
         //mem id / pb no
         $pdf->setColor(0,0,0,1);
         $infoH -= $pdf->C2P(1);
-        $infoW = $startW+$pdf->C2P(0.1);
+        $infoW = $startW+$pdf->C2P(0.5);
         $pdf->addTextWrap($infoW, $infoH, $pdf->C2P(2.2), $fontSize, "<b>MEM ID/PB#:</b>",'left');
-        $pdf->line($infoW+$pdf->C2P(2.2),$infoH-$pdf->C2P(0.1),$infoW+$pdf->C2P(8),$infoH-$pdf->C2P(0.1));
+        $pdf->line($infoW+$pdf->C2P(2.2),$infoH-$pdf->C2P(0.1),$infoW+$pdf->C2P(7.6),$infoH-$pdf->C2P(0.1));
         $infoW += $pdf->C2P(2.3);
         $pdf->addTextWrap($infoW, $infoH, $pdf->C2P(4), $fontSize, "<b>".$ticket->pbno."</b>",'left');
         
         //name
         $pdf->setColor(0,0,0,1);
         $infoH -= $pdf->C2P(0.75);
-        $infoW = $startW+$pdf->C2P(0.1);
+        $infoW = $startW+$pdf->C2P(0.5);
         $pdf->addTextWrap($infoW, $infoH, $pdf->C2P(1.2), $fontSize, "<b>NAME:</b>",'left');
         $infoW += $pdf->C2P(1.3);
         $text = "<b>".$ticket->name."</b>";
@@ -69,14 +71,14 @@
             $text = $pdf->addTextWrap($infoW, $infoH,$pdf->C2P(6.5),$fontSize,$text,'left');
         }
         $infoW -= $pdf->C2P(1.3);
-        $pdf->line($infoW+$pdf->C2P(1.2),$infoH-$pdf->C2P(0.1),$infoW+$pdf->C2P(8),$infoH-$pdf->C2P(0.1));
+        $pdf->line($infoW+$pdf->C2P(1.2),$infoH-$pdf->C2P(0.1),$infoW+$pdf->C2P(7.6),$infoH-$pdf->C2P(0.1));
 
         //contact
         $pdf->setColor(0,0,0,1);
         $infoH -= $pdf->C2P(0.75);
-        $infoW = $startW+$pdf->C2P(0.1);
+        $infoW = $startW+$pdf->C2P(0.5);
         $pdf->addTextWrap($infoW, $infoH, $pdf->C2P(2.3), $fontSize, "<b>CONTACT NO:</b>",'left');
-        $pdf->line($infoW+$pdf->C2P(2.3),$infoH-$pdf->C2P(0.1),$infoW+$pdf->C2P(8),$infoH-$pdf->C2P(0.1));
+        $pdf->line($infoW+$pdf->C2P(2.3),$infoH-$pdf->C2P(0.1),$infoW+$pdf->C2P(7.6),$infoH-$pdf->C2P(0.1));
         $infoW += $pdf->C2P(2.4);
         $pdf->addTextWrap($infoW, $infoH, $pdf->C2P(4), $fontSize, "<b>".$ticket->contact."</b>",'left');
     }
@@ -85,9 +87,8 @@
     
     foreach($ticketList as $ticket){
         $extend = $pdf->C2P(0.015);
-        $pdf->setLineStyle(1);
-        $ticketW = $pdf->C2P(9.5);
-        $rowH = $pdf->C2P(5);
+        $ticketW = $pdf->C2P(9.9);
+        $rowH = $pdf->C2P(4.4);
         $ticketH = $startH;
         $ticketH-=$rowH;
 
