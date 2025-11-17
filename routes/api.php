21b16e0064f19ba6abc84e4c23f78a4236ecc53e
joashcabanilla <joash0199@gmail.com>
2025-11-17 08:22:28 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\AdminController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+//Controllers
+use App\Http\Controllers\DataController;
+
+//import data route
+Route::post("import", [DataController::class, "import"]);
