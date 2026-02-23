92f15c7bef2c362aca615f88ff6b464f73d1452f
joashcabanilla <joash0199@gmail.com>
2026-02-23 11:57:33 +0800

    update election closed

--- resources/views/Components/Guest/ElectionClosed.blade.php
+++ resources/views/Components/Guest/ElectionClosed.blade.php
@@ -6,8 +6,10 @@
                 <img src="{{asset('image/1.png')}}" alt="logo" width="350" />
             </div>
             <div class="card-body">
-                <h1 class="font-weight-bold text-monospace text-center">The 50th General Assembly System is</h1>
-                <h1 class="font-weight-bold text-monospace text-center" style="color:purple">OFFICIALLY CLOSED.</h1> 
+                {{-- <h1 class="font-weight-bold text-monospace text-center">The 50th General Assembly System is</h1>
+                <h1 class="font-weight-bold text-monospace text-center" style="color:purple">OFFICIALLY CLOSED.</h1>  --}}
+                <h1 class="font-weight-bold text-monospace text-center">The MIGS Verifier will open on</h1>
+                <h1 class="font-weight-bold text-monospace text-center" style="color:purple">March 2, 2026.</h1> 
                 {{-- <h1 class="font-weight-bold text-monospace text-center" style="color:purple">UNDER MAINTENANCE.</h1> --}}
                 {{-- <h1 class="font-weight-bold text-monospace text-center">The 50th PRE-GA online registration and voting are</h1>
                 <h1 class="font-weight-bold text-monospace text-center" style="color:purple">accessible only from 7:00 AM to 10:00 PM</h1>  --}}
